7830a1c10b318a62de7cf0c9b7c5964fbf18dd04
adamdon <mail@adamdon.co.uk>
2022-06-22 08:56:30 +0100

    Added handleMethodArgumentNotValid handling.

--- src/main/java/uk/co/adamdon/springbooturlshorter/exceptions/RestExceptionHandler.java
+++ src/main/java/uk/co/adamdon/springbooturlshorter/exceptions/RestExceptionHandler.java
@@ -6,9 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uk.co.adamdon.springbooturlshorter.controllers.LinkController;
 
@@ -46,4 +48,38 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler
 
         return responseEntity;
     }
+
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ResponseEntity<Object> responseEntity;
+        JSONObject jsonObject;
+        String messageFullString;
+        String messageShortString;
+        HttpHeaders httpHeaders;
+
+        messageFullString = ex.getMessage();
+        messageShortString = messageFullString.substring((messageFullString.lastIndexOf("[") + 1), messageFullString.lastIndexOf("]]"));
+
+        jsonObject = new JSONObject();
+        jsonObject.put("status", HttpStatus.BAD_REQUEST);
+        jsonObject.put("message", "Invalid data send");
+        jsonObject.put("error", messageShortString);
+
+        httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-Type", "application/json");
+
+        responseEntity = new ResponseEntity<>(jsonObject.toString(), httpHeaders, HttpStatus.BAD_REQUEST);
+
+        logger.info("handleMethodArgumentNotValid: " + jsonObject.toString());
+
+
+        return responseEntity;
+    }
+
+
+
+
 }

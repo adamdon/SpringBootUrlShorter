06fefa5b7acaedaa0e9661c40609f2e219cce8a7
adamdon <mail@adamdon.co.uk>
2022-08-09 07:32:41 +0100

    Added simple junit test for codeGenerator

--- src/test/java/uk/co/adamdon/springbooturlshorter/SpringBootUrlShorterApplicationTests.java
+++ src/test/java/uk/co/adamdon/springbooturlshorter/SpringBootUrlShorterApplicationTests.java
@@ -2,12 +2,29 @@ package uk.co.adamdon.springbooturlshorter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.co.adamdon.springbooturlshorter.utilities.CodeGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringBootUrlShorterApplicationTests {
 
+
+
 	@Test
-	void contextLoads() {
+	public void testCodeGenerator()
+	{
+		CodeGenerator codeGenerator;
+		String urlString;
+		String expectedCodeString;
+		String actualCodeString;
+
+		codeGenerator = CodeGenerator.getInstance();
+		urlString = "https://gizmodo.com";
+		expectedCodeString = "uGlmsc6fKcxl_Us78GceXA==";
+		actualCodeString = codeGenerator.create(urlString);
+
+		assertEquals(expectedCodeString, actualCodeString);
 	}
 
 }
